8b5fb2a5db424a230d14a12cea0f05f0549d26cb
Richard Foster <richard@madetech.co.uk>
2015-08-02 14:47:21 +0100

    Add template file and a hacky CLI

---
+++ index.js
@@ -0,0 +1,44 @@
+var spawn = require('child_process').spawn
+  , fs = require('fs')
+  , Path = require('path')
+  ;
+
+var flags = process.argv.slice(2).join(' ');
+
+var MAX_LEVELS = (function () {
+  var levels = flags.match(/--levels=(\d*)/);
+  return levels && levels[1] || 2;
+})();
+
+if (MAX_LEVELS >= 5 && flags.indexOf('--seriously') === -1) {
+  console.error('You gotta be serious. Use --seriously if you\'re serious.')
+  process.exit(1);
+}
+
+if (flags.substr(0, 5) === 'setup')
+  setup();
+
+if (flags.substr(0, 8) === 'multiply')
+  multiply(1, 1);
+
+function setup () {
+  var read = fs.createReadStream('./template.js')
+    , write = fs.createWriteStream('./children/1.js');
+
+  read.pipe(write);
+};
+
+function multiply (level, id) {
+  console.log('restarting with ' + id);
+  if (level <= MAX_LEVELS) {
+    var fname = (id || 1) + '.js'
+      , fpath = Path.relative(__dirname, Path.join(__dirname, 'children', fname));
+
+    var cp = spawn('node', [fpath]);
+
+    cp.stdout.on('data', function (children) {
+      children = children.toString().replace(/[^\d,]/g, '').split(',');
+      children.forEach(multiply.bind(null, level + 1));
+    });
+  }
+};
